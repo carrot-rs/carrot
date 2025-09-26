a698eb51591763f6f855533cb40dc91c2d8c82ab
nyxb <dennis@ollhoff.life>
2025-09-26 17:20:18 +0200

    refactor(inazuma): simplify animation

---
+++ crates/inazuma/src/platform/mac/window_appearance.rs
@@ -0,0 +1,23 @@
+use inazuma::WindowAppearance;
+use objc2_app_kit::{
+    NSAppearance, NSAppearanceNameAqua, NSAppearanceNameDarkAqua, NSAppearanceNameVibrantDark,
+    NSAppearanceNameVibrantLight,
+};
+
+pub(crate) fn window_appearance_from_native(appearance: &NSAppearance) -> WindowAppearance {
+    let name = appearance.name();
+    unsafe {
+        if *name == *NSAppearanceNameVibrantLight {
+            WindowAppearance::VibrantLight
+        } else if *name == *NSAppearanceNameVibrantDark {
+            WindowAppearance::VibrantDark
+        } else if *name == *NSAppearanceNameAqua {
+            WindowAppearance::Light
+        } else if *name == *NSAppearanceNameDarkAqua {
+            WindowAppearance::Dark
+        } else {
+            log::warn!("unknown appearance: {:?}", name);
+            WindowAppearance::Light
+        }
+    }
+}
