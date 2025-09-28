ec33886c3285632a040291856d2af69ee3a4a5d7
nyxb <dennis@ollhoff.life>
2025-09-28 18:07:16 +0200

    refactor(inazuma): rename gradient helpers

---
+++ crates/inazuma/src/text_system.rs
@@ -0,0 +1,17 @@
+mod font_fallbacks;
+mod font_features;
+mod font_types;
+mod line;
+mod line_layout;
+mod line_wrapper;
+mod systems;
+mod window_text_system;
+
+pub use font_fallbacks::*;
+pub use font_features::*;
+pub use font_types::*;
+pub use line::*;
+pub use line_layout::*;
+pub use line_wrapper::*;
+pub use systems::*;
+pub use window_text_system::*;
