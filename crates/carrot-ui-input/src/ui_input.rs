9b7f79265d5ebf737b0eb2392fb283be7b132510
nyxb <dennis@ollhoff.life>
2026-01-18 12:54:58 +0200

    feat(ui-input): input field module

---
+++ crates/carrot-ui-input/src/ui_input.rs
@@ -0,0 +1,5 @@
+//! UI components for form-like scenarios (input fields, number fields).
+mod input_field;
+
+pub use carrot_ui::{ERASED_EDITOR_FACTORY, ErasedEditor, ErasedEditorEvent};
+pub use input_field::*;
