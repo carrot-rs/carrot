5127296cfa01b8eb9e52c9735458aae1a022ca9a
nyxb <dennis@ollhoff.life>
2026-01-19 17:59:49 +0200

    feat(ui): add bar chart

---
+++ crates/carrot-ui/src/components/table/data_table.rs
@@ -0,0 +1,22 @@
+use crate::utils::actions::{
+    Cancel, SelectDown, SelectFirst, SelectLast, SelectNextColumn, SelectPageDown, SelectPageUp,
+    SelectPrevColumn, SelectUp,
+};
+use inazuma::{App, KeyBinding};
+
+const CONTEXT: &str = "DataTable";
+pub(super) fn init(cx: &mut App) {
+    cx.bind_keys([
+        KeyBinding::new("escape", Cancel, Some(CONTEXT)),
+        KeyBinding::new("up", SelectUp, Some(CONTEXT)),
+        KeyBinding::new("down", SelectDown, Some(CONTEXT)),
+        KeyBinding::new("left", SelectPrevColumn, Some(CONTEXT)),
+        KeyBinding::new("right", SelectNextColumn, Some(CONTEXT)),
+        KeyBinding::new("home", SelectFirst, Some(CONTEXT)),
+        KeyBinding::new("end", SelectLast, Some(CONTEXT)),
+        KeyBinding::new("pageup", SelectPageUp, Some(CONTEXT)),
+        KeyBinding::new("pagedown", SelectPageDown, Some(CONTEXT)),
+        KeyBinding::new("tab", SelectNextColumn, Some(CONTEXT)),
+        KeyBinding::new("shift-tab", SelectPrevColumn, Some(CONTEXT)),
+    ]);
+}
