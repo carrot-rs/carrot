90ee3d5485f2c7cec6da091da695e203a2b5b1e7
nyxb <dennis@ollhoff.life>
2026-03-21 08:44:41 +0200

    feat(edit-prediction-cli): headless wiring

---
+++ crates/carrot-edit-prediction-cli/src/headless.rs
@@ -0,0 +1,124 @@
+use carrot_client::{Client, ProxySettings, UserStore};
+use carrot_db::AppDatabase;
+use carrot_extension::ExtensionHostProxy;
+use carrot_fs::RealFs;
+use carrot_language::LanguageRegistry;
+use carrot_language_extension::LspAccess;
+use carrot_node_runtime::{NodeBinaryOptions, NodeRuntime};
+use carrot_project::project_settings::ProjectSettings;
+use carrot_release_channel::{AppCommitSha, AppVersion};
+use carrot_reqwest_client::ReqwestClient;
+use inazuma::http_client::read_proxy_from_env;
+use inazuma::{App, AppContext, Entity};
+use inazuma_settings_framework::{Settings, SettingsStore};
+use inazuma_tokio::Tokio;
+use inazuma_util::ResultExt as _;
+use std::path::PathBuf;
+use std::sync::Arc;
+
+/// Headless subset of `workspace::AppState`.
+pub struct EpAppState {
+    pub languages: Arc<LanguageRegistry>,
+    pub client: Arc<Client>,
+    pub user_store: Entity<UserStore>,
+    pub fs: Arc<dyn carrot_fs::Fs>,
+    pub node_runtime: NodeRuntime,
+}
+
+pub fn init(cx: &mut App) -> EpAppState {
+    let app_commit_sha = option_env!("CARROT_COMMIT_SHA").map(|s| AppCommitSha::new(s.to_owned()));
+
+    let app_version = AppVersion::load(
+        env!("CARROT_PKG_VERSION"),
+        option_env!("CARROT_BUILD_ID"),
+        app_commit_sha,
+    );
+    carrot_release_channel::init(app_version.clone(), cx);
+    inazuma_tokio::init(cx);
+
+    let settings_store = SettingsStore::new(cx, &inazuma_settings_framework::default_settings());
+    cx.set_global(settings_store);
+
+    // Set User-Agent so we can download language servers from GitHub
+    let user_agent = format!(
+        "Carrot Edit Prediction CLI/{} ({}; {})",
+        app_version,
+        std::env::consts::OS,
+        std::env::consts::ARCH
+    );
+    let proxy_str = ProxySettings::get_global(cx).proxy.to_owned();
+    let proxy_url = proxy_str
+        .as_ref()
+        .and_then(|input| input.parse().ok())
+        .or_else(read_proxy_from_env);
+    let http = {
+        let _guard = Tokio::handle(cx).enter();
+
+        ReqwestClient::proxy_and_user_agent(proxy_url, &user_agent)
+            .expect("could not start HTTP client")
+    };
+    cx.set_http_client(Arc::new(http));
+
+    let client = Client::production(cx);
+    cx.set_http_client(client.http_client());
+
+    let app_db = AppDatabase::new();
+    cx.set_global(app_db);
+
+    let git_binary_path = None;
+    let fs = Arc::new(RealFs::new(
+        git_binary_path,
+        cx.background_executor().clone(),
+    ));
+
+    let mut languages = LanguageRegistry::new(cx.background_executor().clone());
+    languages.set_language_server_download_dir(carrot_paths::languages_dir().clone());
+    let languages = Arc::new(languages);
+
+    let user_store = cx.new(|cx| UserStore::new(client.clone(), cx));
+
+    carrot_extension::init(cx);
+
+    let (mut tx, rx) = carrot_watch::channel(None);
+    cx.observe_global::<SettingsStore>(move |cx| {
+        let settings = &ProjectSettings::get_global(cx).node;
+        let options = NodeBinaryOptions {
+            allow_path_lookup: !settings.ignore_system_version,
+            allow_binary_download: true,
+            use_paths: settings.path.as_ref().map(|node_path| {
+                let node_path = PathBuf::from(shellexpand::tilde(node_path).as_ref());
+                let npm_path = settings
+                    .npm_path
+                    .as_ref()
+                    .map(|path| PathBuf::from(shellexpand::tilde(&path).as_ref()));
+                (
+                    node_path.clone(),
+                    npm_path.unwrap_or_else(|| {
+                        let base_path = PathBuf::new();
+                        node_path.parent().unwrap_or(&base_path).join("npm")
+                    }),
+                )
+            }),
+        };
+        tx.send(Some(options)).log_err();
+    })
+    .detach();
+    let node_runtime = NodeRuntime::new(client.http_client(), None, rx);
+
+    let extension_host_proxy = ExtensionHostProxy::global(cx);
+
+    carrot_debug_adapter_extension::init(extension_host_proxy.clone(), cx);
+    carrot_language_extension::init(LspAccess::Noop, extension_host_proxy, languages.clone());
+    carrot_language_model::init(user_store.clone(), client.clone(), cx);
+    carrot_language_models::init(user_store.clone(), client.clone(), cx);
+    carrot_languages::init(languages.clone(), fs.clone(), node_runtime.clone(), cx);
+    carrot_prompt_store::init(cx);
+
+    EpAppState {
+        languages,
+        client,
+        user_store,
+        fs,
+        node_runtime,
+    }
+}
