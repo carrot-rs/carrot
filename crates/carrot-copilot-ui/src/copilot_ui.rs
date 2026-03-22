397e833fbc6e5441f1c82f002b7bc60fe1c0fb60
nyxb <dennis@ollhoff.life>
2026-03-22 10:20:08 +0200

    refactor(copilot-ui): simplify copilot ui

---
+++ crates/carrot-copilot-ui/src/copilot_ui.rs
@@ -0,0 +1,35 @@
+mod sign_in;
+
+use std::sync::Arc;
+
+use carrot_copilot::GlobalCopilotAuth;
+use carrot_language::language_settings::{AllLanguageSettings, EditPredictionProvider};
+use carrot_project::DisableAiSettings;
+use carrot_ui::App;
+use carrot_workspace::AppState;
+use inazuma::AppContext;
+use inazuma_settings_framework::SettingsStore;
+pub use sign_in::{
+    ConfigurationMode, ConfigurationView, CopilotCodeVerification, initiate_sign_in,
+    initiate_sign_out, reinstall_and_sign_in,
+};
+
+pub fn init(app_state: &Arc<AppState>, cx: &mut App) {
+    let disable_ai = cx.read_global(|settings: &SettingsStore, _| {
+        settings.get::<DisableAiSettings>(None).disable_ai
+    });
+    let provider = cx.read_global(|settings: &SettingsStore, _| {
+        settings
+            .get::<AllLanguageSettings>(None)
+            .edit_predictions
+            .provider
+    });
+    if !disable_ai && provider == EditPredictionProvider::Copilot {
+        GlobalCopilotAuth::set_global(
+            app_state.languages.next_language_server_id(),
+            app_state.fs.clone(),
+            app_state.node_runtime.clone(),
+            cx,
+        );
+    }
+}
